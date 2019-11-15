20af5422b5c97233bfc459f7fb837fe301b50bd9
Klaus Buecher <opto@optosolar.com>
2019-11-15 13:28:00 +0100

    give reference to restart-application-1.2.1

--- chrome/content/just-restart.js
+++ chrome/content/just-restart.js
@@ -6,7 +6,7 @@
 
 function jrExec(){
 
-
+//borrowed from restart-application-1.2.1 
 var boot=Components.classes['@mozilla.org/toolkit/app-startup;1'].getService(Components.interfaces.nsIAppStartup); 
 boot.quit(Components.interfaces.nsIAppStartup.eForceQuit|Components.interfaces.nsIAppStartup.eRestart);  let {BrowserUtils} = ChromeUtils.import ("resource://gre/modules/BrowserUtils.jsm", {});
 //  BrowserUtils.restartApplication();
